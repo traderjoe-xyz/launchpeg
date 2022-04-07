a283330376a2c6fbb29a01a6bb7719616a8b9beb
0xmaxime <Max-3-7@users.noreply.github.com>
2022-04-07 22:56:58 +0200

    Cleanup tests

--- test/utils/helpers.ts
+++ test/utils/helpers.ts
@@ -1,14 +1,45 @@
 import { ethers } from 'hardhat'
 import { BigNumber, Contract } from 'ethers'
-import { duration, advanceTimeAndBlock } from './time'
+import { duration, advanceTimeAndBlock, latest } from './time'
 
-export const LAUNCHPEG_CONFIG = {
-  startPrice: ethers.utils.parseUnits('1', 18),
-  endPrice: ethers.utils.parseUnits('0.15', 18),
-  auctionPriceCurveLength: duration.minutes(340),
-  auctionDropInterval: duration.minutes(20),
-  mintlistDiscount: 0.1 * 10000,
-  publicSaleDiscount: 0.2 * 10000,
+export interface LaunchPegConfig {
+  auctionStartTime: BigNumber
+  mintlistStartTime: BigNumber
+  publicSaleStartTime: BigNumber
+  maxBatchSize: number
+  collectionSize: number
+  amountForAuction: number
+  amountForMintlist: number
+  amountForDevs: number
+  startPrice: BigNumber
+  endPrice: BigNumber
+  auctionPriceCurveLength: BigNumber
+  auctionDropInterval: BigNumber
+  mintlistDiscount: number
+  publicSaleDiscount: number
+}
+
+const MINTLIST_START_OFFSET = 10
+const PUBLIC_SALE_START_OFFSET = 20
+
+export const getDefaultLaunchPegConfig = async (): Promise<LaunchPegConfig> => {
+  const auctionStartTime = await latest()
+  return {
+    auctionStartTime,
+    mintlistStartTime: auctionStartTime.add(duration.minutes(MINTLIST_START_OFFSET)),
+    publicSaleStartTime: auctionStartTime.add(duration.minutes(PUBLIC_SALE_START_OFFSET)),
+    maxBatchSize: 5,
+    collectionSize: 10000,
+    amountForAuction: 8000,
+    amountForMintlist: 1900,
+    amountForDevs: 100,
+    startPrice: ethers.utils.parseUnits('1', 18),
+    endPrice: ethers.utils.parseUnits('0.15', 18),
+    auctionPriceCurveLength: duration.minutes(340),
+    auctionDropInterval: duration.minutes(20),
+    mintlistDiscount: 0.1 * 10000,
+    publicSaleDiscount: 0.2 * 10000,
+  }
 }
 
 export enum Phase {
@@ -17,17 +48,17 @@ export enum Phase {
   PublicSale,
 }
 
-export const initializePhases = async (launchPeg: Contract, auctionStartTime: BigNumber, currentPhase: Phase) => {
+export const initializePhases = async (launchPeg: Contract, config: LaunchPegConfig, currentPhase: Phase) => {
   await launchPeg.initializePhases(
-    auctionStartTime,
-    LAUNCHPEG_CONFIG.startPrice,
-    LAUNCHPEG_CONFIG.endPrice,
-    LAUNCHPEG_CONFIG.auctionPriceCurveLength,
-    LAUNCHPEG_CONFIG.auctionDropInterval,
-    auctionStartTime.add(duration.minutes(10)),
-    LAUNCHPEG_CONFIG.mintlistDiscount,
-    auctionStartTime.add(duration.minutes(20)),
-    LAUNCHPEG_CONFIG.publicSaleDiscount
+    config.auctionStartTime,
+    config.startPrice,
+    config.endPrice,
+    config.auctionPriceCurveLength,
+    config.auctionDropInterval,
+    config.mintlistStartTime,
+    config.mintlistDiscount,
+    config.publicSaleStartTime,
+    config.publicSaleDiscount
   )
   await advanceTimeAndBlockToPhase(currentPhase)
 }
@@ -37,10 +68,10 @@ const advanceTimeAndBlockToPhase = async (phase: Phase) => {
     case Phase.DutchAuction:
       break
     case Phase.Mintlist:
-      await advanceTimeAndBlock(duration.minutes(10))
+      await advanceTimeAndBlock(duration.minutes(MINTLIST_START_OFFSET))
       break
     case Phase.PublicSale:
-      await advanceTimeAndBlock(duration.minutes(20))
+      await advanceTimeAndBlock(duration.minutes(PUBLIC_SALE_START_OFFSET))
       break
   }
 }
